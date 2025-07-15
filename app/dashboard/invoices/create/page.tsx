cccb5799441ddc1dada5c37c0cfb997cf81daefb
Dirga36 <fuaddabbad@gmail.com>
2025-07-15 11:00:04 +0700

    chapter 12

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,23 +1,28 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- 
+import Layout from '../../dashboard/layout'
+import { createInvoice } from '@/app/lib/actions';
+
 export default async function Page() {
   const customers = await fetchCustomers();
- 
+
   return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
-          {
-            label: 'Create Invoice',
-            href: '/dashboard/invoices/create',
-            active: true,
-          },
-        ]}
-      />
-      <Form customers={customers} />
-    </main>
+    <Layout>
+      <form action={createInvoice} />
+      <main>
+        <Breadcrumbs
+          breadcrumbs={[
+            { label: 'Invoices', href: '/dashboard/invoices' },
+            {
+              label: 'Create Invoice',
+              href: '/dashboard/invoices/create',
+              active: true,
+            },
+          ]}
+        />
+        <Form customers={customers} />
+      </main>
+    </Layout>
   );
 }
